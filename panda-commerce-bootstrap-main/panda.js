024dbf5f2355aff09d166bbc143c1e8e8b8267aa
Mst Rinky Akter <mstrinkya@gmail.com>
2023-08-14 22:24:51 +0600

    problem

--- panda-commerce-bootstrap-main/panda.js
+++ panda-commerce-bootstrap-main/panda.js
@@ -1,5 +1,23 @@
 const h2 = document.querySelectorAll('h2');
 for(const allH2 of h2){
-    console.log(allH2.innerText);
+    // console.log(allH2.innerText);
+    allH2.style.color = 'lightBlue';
 }
 
+
+// const changeBackground = document.getElementById('backpack');
+// for(const section of changeBackground){
+//   console.log(section.innerText);
+// }
+
+
+document.getElementById('exampleInputEmail1').addEventListener('keyup',function(event){
+   const text = event.target.value ;
+  const submit = document.getElementById('submit-btn');
+   if(text === "email"){
+    submit.removeAttribute = ('disabled');
+   }
+   else{
+    submit.setAttribute = ('disabled',true);
+   }
+})
